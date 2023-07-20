a1fbd3cfc4e7cbdb8e10fdbf28608d8419fb14de
tuna7797 <gmlwjd7731@gmail.com>
2023-07-20 10:14:04 +0900

    add. pagination 변경

--- client/js/part03.js
+++ client/js/part03.js
@@ -21,3 +21,20 @@ const data = [
   },
 ];
 
+const swiper = new Swiper(".swiper", {
+  autoplay: true,
+  loop: true,
+  pagination: {
+    el: ".pagination",
+    clickable: true,
+    bulletClass: "bullet",
+    bulletActiveClass: "is-active",
+    renderBullet: function (index, className) {
+      return /* html */ `
+        <span class="${className}">
+          <img src="./assets/part01/${data[index].src}" alt="" />
+        </span>
+      `;
+    },
+  },
+});
