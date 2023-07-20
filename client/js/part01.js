7a7979a875a4b5e22dafc44583b7b441cab6213e
tuna7797 <gmlwjd7731@gmail.com>
2023-07-20 10:13:32 +0900

    1일차 수업

--- client/js/part01.js
+++ client/js/part01.js
@@ -25,9 +25,52 @@ const data = [
 ]
 
 
-// 내가 선택한 li에게 is-active 클래스를 넣어주세요 
+// 1. navigation을 선택 후 이벤트 위임을 걸어주세요.
+// 2. target은 li요소가 잘 나와야 합니다.
 
-// 내가 선택하지 않은(모든 li) li에게 is-active 클래스 제거해주세요 
+
+
+const navigation = getNode('.navigation');
+// const list = getNodes('.navigation li');
+const visualImage = getNode('.visual img');
+
+// querySelectorAll
+// elementNode.children
+
+function handleSlider(e){
+  e.preventDefault();
+  
+  const target = e.target.closest('li');
+  const anchor = e.target.closest('a');
+  
+  if(!target || !anchor) return;
+
+  
+  const list = [...navigation.children];
+  const index = attr(target,'data-index');
+
+
+  list.forEach(li => removeClass(li,'is-active'));
+
+
+  addClass(target,'is-active')
+
+  attr(visualImage,'src',`./assets/part01/${data[index -1].src}`)
+  attr(visualImage,'alt',data[index -1].alt)
+
+  // visualImage.setAttribute('src',`./assets/part01/${data[index -1].src}`);
+  // visualImage.setAttribute('alt',data[index -1].alt);
+
+}
+
+
+
+navigation.addEventListener('click',handleSlider);
+
+
+
+
+
 
 // 선택한 대상의 data-index값을 가져와주세요.
 
@@ -36,6 +79,15 @@ const data = [
 // 이미지의 src 속성에 접근한다.
 // src의 값을 index로 바꾼다.
 // alt 값을 변경한다.
+
+
+
+// 화살표 함수 쓰세요 ! 
+
+
+
+
+
 
 
 
